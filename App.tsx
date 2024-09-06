b52d259fefd972996952c79b26faf23fff104f03
Ömer Faruk Türkkal <oft2018oft@gmail.com>
2024-09-06 11:13:10 +0300

    changed

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tking on your app!</Text>
+      <Text>Helsfdld</Text>
       <StatusBar style="auto" />
     </View>
   );
